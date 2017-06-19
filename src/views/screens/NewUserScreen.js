5feb62a4bc5bedddff26d1e6450edd82e76dc9ab
CherylHuang <jjccbb112233@gmail.com>
2017-06-20 01:14:46 +0800

    Add files via upload

--- src/views/screens/NewUserScreen.js
+++ src/views/screens/NewUserScreen.js
@@ -1,26 +1,8 @@
 import React, { Component } from 'react';
 import { View, Picker, ActivityIndicator, ScrollView, Text } from 'react-native';
 import * as firebase from 'firebase';
+
 import { FormLabel, FormInput, Button, CheckBox } from 'react-native-elements';
-
-const styles = {
-  formStyle: {
-    marginTop: 50
-  },
-  pickerStyle:{
-    marginTop:-30,
-    marginBottom:-30
-  },
-  textCancel:{
-    color:'#bbbbbb',
-    textDecorationLine:'underline',
-    alignSelf:"center",
-    fontSize:15,
-    marginTop:30,
-    marginBottom:50
-  }
-};
-
 
 // Make a component
 class NewUserScreen extends Component {
@@ -41,7 +23,7 @@ class NewUserScreen extends Component {
       let dbUserid = firebase.database().ref(`/users/${currentUser.uid}`);
       await dbUserid.set({ email });
 
-      this.props.navigation.navigate('TabRouter');
+      // this.props.navigation.navigate('AccountStack');
       this.setState({ saving: false });
     } catch (err) {
       this.setState({
@@ -53,54 +35,55 @@ class NewUserScreen extends Component {
     }
   }
 
-  renderButton(navigation) {
+  renderButton() {
     if (this.state.saving) {
       return <ActivityIndicator size='large' style={{marginTop:30}} />;
     }
 
     return (
       <Button
-        title='建立並登入'
+        title='Sign up'
+        onPress={this.onSaveInfo}
         backgroundColor='#4AAF4C'
-        borderRadius={5}
-        fontSize={18}
         buttonStyle = {{marginTop:30}}
-        onPress={() => this.props.navigation.dispatch({ type: 'Login' })}
-        />
+      />
     );
   }
 
+  onCancel = () => {
+    this.props.navigation.navigate('LoginScreen');
+  }
 
   render() {
-    const navigation = this.props.navigation;
+    console.log(this.state);
     return (
       <ScrollView>
       <View style={styles.formStyle}>
-        <FormLabel labelStyle = {{fontSize:15}}>帳號信箱</FormLabel>
+        <FormLabel>Email</FormLabel>
         <FormInput
           placeholder='user@email.com'
           autoCorrect={false}
           autoCapitalize='none'
           keyboardType='email-address'
-          /*onChangeText={email => this.setState({ email })}*/
+          onChangeText={email => this.setState({ email })}
         />
-        <FormLabel labelStyle = {{fontSize:15}}>密碼</FormLabel>
+        <FormLabel>Password</FormLabel>
           <FormInput
             secureTextEntry
             autoCorrect={false}
             autoCapitalize='none'
             placeholder='password'
             value={this.state.password}
-            /*onChangeText={password => this.setState({ password })}*/
+            onChangeText={password => this.setState({ password })}
           />
 
-        {this.renderButton(navigation)}
+        {this.renderButton()}
 
-          <Text
+        <Text
            style={styles.textCancel}
-           onPress={ () => this.props.navigation.navigate('Login') }>
-                取消
-          </Text>
+           onPress={this.onCancel}>
+                Cancel
+           </Text>
 
       </View>
       </ScrollView>
@@ -108,5 +91,22 @@ class NewUserScreen extends Component {
   }
 }
 
+const styles = {
+  formStyle: {
+    marginTop: 50
+  },
+  pickerStyle:{
+    marginTop:-30,
+    marginBottom:-30
+  },
+  textCancel:{
+    color:'#bbbbbb',
+    textDecorationLine:'underline',
+    alignSelf:"center",
+    fontSize:15,
+    marginTop:30,
+    marginBottom:50
+  }
+};
 
 export default NewUserScreen;
