5ddf8abb45376055ba4139873a8ef98c51a99590
CherylHuang <jjccbb112233@gmail.com>
2017-06-20 00:43:29 +0800

    Add files via upload

--- src/views/pages/MedicineEdit.js
+++ src/views/pages/MedicineEdit.js
@@ -1,112 +1,79 @@
 import React, { Component } from 'react';
 import { View, Image, Text, ScrollView } from 'react-native';
 import { List, ListItem, Icon } from 'react-native-elements';
-import { connect } from 'react-redux';
 
-// import medi from '../../reducers/json/medicine.json';
+import medicine from '../../reducers/json/medicine.json';
 
 // Make a component
 class MedicineEdit extends Component {
-  // state = { medi: [] };
+  state = { medicine: [] };
 
-  // componentWillMount(newProps) {
-    // this.setState({ medi });
-  // }
-
-  componentWillReceiveProps(nextProps) {
-    let data = nextProps.data;
-    if (nextProps.data !== this.props.medi) {
-      // let data = this._getListViewData(nextProps.medi);
-      data = data.map((medicine) => {
-        return medicine;
-    });
-      this.setState({
-        medi: this.state.dataSource.cloneWithRowsAndSections(data)
-      })
-    }
+  componentWillMount() {
+    this.setState({ medicine });
   }
 
-  //  _getListViewData(medi) {
-  //   let data = {};
-    
-  //   medi.map((patient) => {
-  //     let section = patient.lastName.charAt(0);
-  //     if (sectionIds.indexOf(section) === -1) {
-  //       sectionIds.push(section);
-  //       data[section] = [];
-  //     }
-  //     data[section].push(patient);
-  //   });
-
-  //   return data;
-  // }
-
   render() {
-        const { flex, img, icon, icon_pos, title, righttitle } = styles;
-        const { medi } = this.props;
+        const { img } = styles;
     return (
       <ScrollView>
-          <View style={flex}>
-            <Image source={{uri: medi.image }} 
+          <View>
+            <Image source={{uri: 'http://img01.ibnlive.in/ibnlive/uploads/875x584/jpg/2016/12/medicines1.jpg'}} 
             style={img}
             />
-            <View style={icon_pos}>
-                <Icon name='photo-camera' color='white'
-                underlayColor='#b9d0e3' size={25}
-                containerStyle={icon}
-                onPress={()=>{}} />
-            </View>
           </View>
 
         <List>
           <ListItem
             title={
             <View style={{flexDirection:'row'}}>
-                <Text style={title}>  名稱   </Text>
-                <Text style={{fontSize:17, color: '#bdc6cf'}}>{medi.title} </Text>
+                <Text style={{fontSize:20}}>  名稱   </Text>
+                <Text style={{fontSize:20, color: '#bdc6cf'}}>感冒藥 </Text>
                 <Icon name='edit' color='#517fa4' onPress={()=>{}} />
             </View>
                 }
+            /*rightTitle={'感冒藥'}
+            rightTitleStyle={{fontSize:17}}*/
             hideChevron
           />
           <ListItem
             title="分類"
-            titleStyle={title}
-            /*rightTitle={`${medi.kind.IO}/${medi.kind.WE}/${medi.kind.type}`}*/
-            rightTitleStyle={righttitle}
+            titleStyle={{fontSize:20}}
+            rightTitle={'內服/西藥/膠囊'}
+            rightTitleStyle={{fontSize:17}}
           />
           <ListItem
             title="時段"
-            titleStyle={title}
+            titleStyle={{fontSize:20}}
             rightTitle={'早/中/晚/睡前'}
-            rightTitleStyle={righttitle}
+            rightTitleStyle={{fontSize:17}}
           />
           <ListItem
             title="效用"
-            titleStyle={title}
-            rightTitle={medi.effect}
-            rightTitleStyle={righttitle}
+            titleStyle={{fontSize:20}}
+            rightTitle={'咳嗽 流鼻水'}
+            rightTitleStyle={{fontSize:17}}
           />
           <ListItem
+            key={this.state.medicine.kind}
             title="副作用"
-            titleStyle={title}
-            rightTitle={medi.subeffect}
-            rightTitleStyle={righttitle}
+            titleStyle={{fontSize:20}}
+            rightTitle={'嗜睡'}
+            rightTitleStyle={{fontSize:17}}
           />
           <ListItem
+            key={this.state.medicine.kind}
             title="注意事項"
-            rightTitle={medi.warn}
-            titleStyle={title}
+            titleStyle={{fontSize:20}}
           />
           <ListItem
+            key={this.state.medicine.kind}
             title="本次服用原因"
-            rightTitle={medi.reason}
-            titleStyle={title}
+            titleStyle={{fontSize:20}}
           />
           <ListItem
+            key={this.state.medicine.kind}
             title="備註"
-            rightTitle={medi.other}
-            titleStyle={title}
+            titleStyle={{fontSize:20}}
           />
         </List>
        
@@ -115,41 +82,14 @@ class MedicineEdit extends Component {
   }
 }
 const styles = {
-  flex:{
-    flex:1
-  },
-  icon_pos:{
-    alignSelf:'center',
-    width:130
-  },
   img:{
     alignSelf:'center',
     width:150,
     height:150,
     borderRadius:75,
-    marginTop:30
-  },
-  icon:{
-    alignSelf:'flex-end',
-    marginTop:-45,
-    backgroundColor:'#517fa4',
-    width:40,
-    height:40,
-    borderRadius:20
-  },
-  title:{
-    fontSize:18
-  },
-  righttitle:{
-    fontSize:15
+    marginTop:30,
+    marginBottom:20
   }
 }
 
-// export default MedicineEdit;
-
-const mapStateToProps = state => {
-  return { medi: state.medi }; //let state.libraries turn to props of Medicine 
-};
-
-// export default Medicine;
-export default connect(mapStateToProps)(MedicineEdit);
+export default MedicineEdit;
